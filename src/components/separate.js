68d3b9a929ee449a3253396c5c27d3316fe04763
Horusateel <moha3zt@yahoo.com>
2024-05-10 04:52:46 +0200

    deleted all links and put a placeholder

--- src/components/separate.js
+++ src/components/separate.js
@@ -2,7 +2,7 @@ export default [
     {
         title: "The pyramids",
         location: "Egypt",
-        googleMapsUrl: "https://www.google.com/maps/place/The+Great+Pyramid+of+Giza/@29.9791054,31.1345801,18z/data=!4m6!3m5!1s0x14584587ac8f291b:0x810c2f3fa2a52424!8m2!3d29.9791705!4d31.1342046!16zL20vMDM2bWs?entry=ttu",
+        googleMapsUrl: "#",
         startDate: "DD-MM-YYYY",
         endDate: "DD-MM-YYYY",
         description: "Being one of the seven wonders of the ancient world, the pyramids are truly once in a lifetime experience.",
@@ -11,7 +11,7 @@ export default [
     {
         title: "Jabal Mousa",
         location: "Egypt",
-        googleMapsUrl: "s",
+        googleMapsUrl: "#",
         startDate: "DD-MM-YYYY",
         endDate: "DD-MM-YYYY",
         description: "One of the calmest and heavenly places on Earth.",
@@ -20,7 +20,7 @@ export default [
     {
         title: "The Hanging Church",
         location: "Egypt",
-        googleMapsUrl: "s",
+        googleMapsUrl: "#",
         startDate: "DD-MM-YYYY",
         endDate: "DD-MM-YYYY",
         description: "Among the earliest churches in Cairo, and a pillar of Christianity in Egypt.",
